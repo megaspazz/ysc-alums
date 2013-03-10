86087b5bcac7b63b0b0274c4e0f8c6bff0934e6e
Erik Pejka <tom.ed.awe.67@gmail.com>
2013-03-10 04:56:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YscAlums::Application.config.secret_token = '75d47c6fe0b9a6bf8601ae148a6a46fe83f0eee3c006d18545885c8e00ded3aeb28d589a4666f09fbe3a27abe4e899cc408866e5ab50573f657b4525e4d3ee75'
