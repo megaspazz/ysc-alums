442599d7cfd3e086ddeb57773fffb13931e61f10
Eric Pan <erzpan@gmail.com>
2017-03-18 13:59:46 -0700

    only allow Yale email addresses to register

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,9 @@ class User < ActiveRecord::Base
 
   validates(:name, :presence => true, :length => {:maximum => 50}, :if => :val_name)
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates(:email, :presence => true, :uniqueness => { :case_sensitive => false }, :format => { :with => VALID_EMAIL_REGEX }, :if => :val_email)
+  VALID_YALE_EMAIL_REGEX = /\A[\w+\-.]+@(aya\.)?yale\.edu\z/i
+  
+  validates(:email, :presence => true, :uniqueness => { :case_sensitive => false }, :format => { :with => VALID_YALE_EMAIL_REGEX }, :if => :val_email)
   validates(:password, :presence => true, :length => { :minimum => 6 }, :if => :val_password)
   validates(:password_confirmation, :presence => true, :if => :val_password)
   
